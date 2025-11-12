a1d12edb9d8df74c86eb3ffc5a7b5d01bde98956
Tino <larsen2099@hotmail.com>
2025-11-12 12:04:03 +0100

    database uden env

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
+
+
+const personDatabase = ("/database/personDatabase");
+
+
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/database/personDatabase")
---
+++ database/personDatabase.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const mongoose = require ("mongoose");
+require("dotenv").config();
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected")).
+catch(err => console.log(err));
+
